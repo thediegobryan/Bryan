1fad511140e4a12d9d67c22c364899a921be3696
thediegobryan <thediegobryan@gmail.com>
2023-11-23 12:42:38 -0600

    finished unscrambler

---
+++ script.js
@@ -0,0 +1,40 @@
+let originalString = '';
+let currentString = '';
+let guessingString ;
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('guessLetter').addEventListener('keypress', (event) => {
+        updateString(event.key);
+    });
+});
+
+function initializeString() {
+    originalString = document.getElementById('inputString').value.toLowerCase();
+    currentString = originalString;
+    guessingString = originalString.split("")
+    updateDisplay();
+}
+
+function updateDisplay() {
+    currentString = originalString;
+    guessingString = originalString.split("")
+    document.getElementById('currentString').textContent = currentString;
+    document.getElementById('GuessingString').textContent = guessingString;
+    console.log(guessingString)
+}
+
+function updateString(letterToRemove) {
+    console.log(letterToRemove)
+    const index = guessingString.indexOf(letterToRemove);
+    console.log(index)
+    // If the letter is found, remove it
+    if (index > -1) {
+        guessingString.splice(index, 1);
+    }
+    console.log(guessingString)
+    document.getElementById('GuessingString').textContent = guessingString;
+}
+
+function resetString() {
+    updateDisplay();
+}
